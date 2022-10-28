ff346e5a1ede17667f1bec1fce40a03bbe8313b6
jeff <jeffreyjohn102@gmail.com>
2022-10-28 12:02:02 -0400

    Add css and style loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,18 @@ module.exports = {
         bundle: path.resolve(__dirname, 'src/index.js'),
     },
     devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                // include: /stylesheets|node_modules/,
+                use: ['style-loader', 'css-loader'],
+            }
+        ],
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
