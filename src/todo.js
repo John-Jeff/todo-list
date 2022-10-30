8277f17577ef145cdf782cd9316a1e3e116bf106
jeff <jeffreyjohn102@gmail.com>
2022-10-29 21:42:04 -0400

    Initial script for todo items

---
+++ src/todo.js
@@ -0,0 +1,19 @@
+class Todos {
+    constructor(name, description, dueDate, priority) {
+        this.name = name;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    };
+};
+
+const projects = {};
+
+export function createTodo(title, name, desc, dueDate, prio) {
+    projects[title] = new Todos(name, desc, dueDate, prio);
+}
+
+createTodo('Task One', 'Task One', 'This is the first task', '12/1/22', 3);
+createTodo('Task Two', 'Task Two', 'This is the second task', '12/3/22', 2);
+
+console.log(projects);
