c632295d80b4215125b996f2bad26cd4558e4f46
jeff <jeffreyjohn102@gmail.com>
2022-11-01 20:19:02 -0400

    Create function to create todo elements

--- src/todo.js
+++ src/todo.js
@@ -13,6 +13,32 @@ export function createTodo(title, name, desc, dueDate, prio) {
     projects[title] = new Todos(name, desc, dueDate, prio);
 };
 
+export function createTodoElement(name, dueDate, desc) {
+    const todo = document.createElement('li');
+    todo.classList.add('todo');
+
+    const h3 = document.createElement('h3');
+    h3.textContent = name;
+    h3.classList.add('todo-name');
+
+    const h4 = docuement.createElement('h4');
+    h4.textContent = dueDate;
+    h4.classList.add('todo-date');
+
+    const p = document.createElement('p');
+    p.textContent = desc;
+    p.classList.add('todo-desc');
+
+    // const deleteButton = document.createElement('div');
+    // deleteButton.classList.add('delete-todo');
+
+    todo.append(h3);
+    todo.append(h4);
+    todo.append(p);
+
+    return todo;
+}
+
 createTodo('Task One', 'Task One', 'This is the first task', '12/1/22', 3);
 createTodo('Task Two', 'Task Two', 'This is the second task', '12/3/22', 2);
 
