6238ccdefaeb32f98f95e083dde61c41f27600f8
Jeff <jeffreyjohn102@gmail.com>
2023-04-02 18:25:05 -0400

    Create project form component

---
+++ src/components/project/ProjectForm.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="new-project">
+        <div id="new-project-form">
+            <input id="new-project-input" type="text" placeholder="project name" required>
+            <button id="create-project">create</button>
+            <button id="cancel-project">cancel</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped></style>
